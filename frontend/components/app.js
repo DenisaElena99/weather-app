831cfb9c178a2b6fbc0e2a6e02b95370e52c8aec
Phyramid <denisa.stirbu@gmail.com>
2021-03-31 11:04:48 +0300

    Need to add current city

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -5,7 +5,8 @@ var App = {
   },
   data() {
     return {
-      cities : ["Basel", "New York"],
+      cities : [],
+      citySearch: "",
       currentCity: {
         cityName: "",
         countryCode: "",
@@ -19,7 +20,6 @@ var App = {
         detailSeen: false,
       },
       newCity: {
-        citySearch: "",
         cityName: "",
         countryCode: "",
         feelsLikeTemperature: 0,
@@ -35,61 +35,70 @@ var App = {
       myWeatherCities: [],
     }
   },
-  methods: {
-    // getCurrentWeather: async function () {
-    //
-    //   const URL = `http://localhost:5000/api/currentCity`;
-    //   try {
-    //     const response = await fetch(URL);
-    //     const data = await response.json();
-    //     this.currentCity.cityName = data.cityName;
-    //     // this.cities.push(this.currentCity.cityName);
-    //     this.currentCity.country = data.countryCode;
-    //     this.currentCity.feelsLike = data.feelsLikeTemperature;
-    //     this.currentCity.temperature = data.temperature;
-    //     this.currentCity.weatherDescription = data.weatherDescription;
-    //     this.currentCity.main = data.main;
-    //     this.currentCity.airDescription = data.valueIndex;
-    //     this.currentCity.components = data.componentsList;
-    //
-    //     var time = new Date();
-    //     this.currentCity.myTime = time.toLocaleString('en-US', { hour: 'numeric', hour12: true });
-    //
-    //     // this.cities.push(this.currentCity);
-    //
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // },
 
-    // getSearchCityWeather: async function () {
-    //   const URL = `http://localhost:5000/api/city?q=`;
-    //   const citiess= this.cities.map(city =>
-    //     fetch(URL + city, { method: "GET" }).then(res => res.json())
-    //   );
-    //   this.cities.push(this.newCity.citySearch);
-    //   try {
-    //     const response = await fetch(URL);
-    //     const data = await response.json();
-    //     this.newCity.citySearch = "";
-    //     this.newCity.cityName = data.cityName;
-    //     this.newCity.country = data.countryCode;
-    //     this.newCity.feelsLike = data.feelsLikeTemperature;
-    //     this.newCity.temperature = data.temperature;
-    //     this.newCity.weatherDescription = data.weatherDescription;
-    //     this.newCity.airDescription = data.valueIndex;
-    //     this.newCity.components = data.componentsList;
-    //     this.newCity.myUnixTime = data.myUnixTime;
-    //     this.newCity.main = data.main;
-    //
-    //     // this.cities.push({...this.newCity});
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // },
+
+  methods: {
+    getCurrentWeather: async function () {
+
+      const URL = `http://localhost:5000/api/currentCity`;
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        this.currentCity.cityName = data.cityName;
+        // this.cities.unshift(this.currentCity.cityName);
+        // this.cities.push(this.currentCity.cityName);
+        this.myWeatherCities.push(data);
+        var time = new Date();
+        this.currentCity.myTime = time.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+
+        // this.cities.push(this.currentCity);
+
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    getSearchCityWeather: async function () {
+      const URL = `http://localhost:5000/api/city?q=${this.citySearch}`;
+      console.log(this.citySearch);
+      this.cities.push(this.citySearch);
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        this.citySearch = "";
+        this.newCity = data;
+        this.myWeatherCities.push({...this.data});
+      } catch (error) {
+        console.log(error);
+      }
+
+      this.getMyCities();
+    },
+
+
+    getMyCities() {
+      var res = "";
+      const URL = `http://localhost:5000/api/city?q=`;
+      const citiesDetails= this.cities.map(city =>
+        fetch(`${URL}${city}`, { method: "GET" }).then(res => res.json())
+      );
+      Promise
+        .all(citiesDetails)
+        .then(citiesData => {
+          this.myWeatherCities = citiesData;
+          console.log(this.myWeatherCities);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+        console.log(this.cities);
+    },
+
 
     deleteCity(index) {
       this.cities.splice(index, 1);
+
+      this.getMyCities();
     },
   },
 
@@ -103,30 +112,17 @@ var App = {
     }
   },
 
+
   beforeMount() {
-       if (window.localStorage.getItem('city-list')) {
-         this.cities = JSON.parse(window.localStorage.getItem('city-list' || '[]'));
-       }
-     },
+     if (window.localStorage.getItem('city-list')) {
+       this.cities = JSON.parse(window.localStorage.getItem('city-list' || '[]'));
+     }
+  },
 
 
-   mounted() {
+  mounted() {
    this.$nextTick(function () {
-     var res = "";
-     const URL = `http://localhost:5000/api/city?q=`;
-     const citiesDetails= this.cities.map(city =>
-       fetch(`${URL}${city}`, { method: "GET" }).then(res => res.json())
-     );
-     Promise
-       .all(citiesDetails)
-       .then(citiesData => {
-         this.myWeatherCities = citiesData;
-         console.log(this.myWeatherCities);
-       })
-       .catch(error => {
-         console.log(error);
-       });
-       console.log(this.cities);
+     this.getMyCities();
     })
   },
 };
